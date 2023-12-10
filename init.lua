2be709771e4d68e343c83d184aa24939c197cd7d
remnantofcliff <ville.nieminen0@gmail.com>
2023-12-10 13:42:57 +0200

    DAP

--- init.lua
+++ init.lua
@@ -161,6 +161,73 @@ require('lazy').setup({
 
     },
   },
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    event = "VeryLazy",
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_setup = true,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
+
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          -- Update this to ensure that you have the debuggers for the langs you want
+        },
+      }
+
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Breakpoint' })
+
+      -- Dap UI setup
+      -- For more information, see |:help nvim-dap-ui|
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      -- Install golang specific config
+      -- require('dap-go').setup()
+    end,
+  }
 }, {
   defaults = {
     lazy = true,
@@ -224,6 +291,7 @@ vim.o.wrap = false                   -- Disable line wrap
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.expandtab = true
+vim.cmd("let g:c_syntax_for_h = 1")
 
 
 -- [[ Basic Keymaps ]]
@@ -469,18 +537,23 @@ require('mason').setup()
 require('mason-lspconfig').setup()
 
 local servers = {
-  clangd = {},
+  clangd = {
+
+  },
   -- gopls = {},
   -- pyright = {},
   rust_analyzer = {
-    ["rust_analyzer"] = {
+    ["rust-analyzer"] = {
       cargo = {
         buildScripts = {
           enable = true,
         },
       },
-      check = {
+      checkOnSave = {
         command = "clippy",
+      },
+      diagnostics = {
+        enable = true,
       },
       procMacro = {
         enable = true
