f4059000d2cbef82fa7affaea80974b0f2bd3a4f
remnantofcliff <ville.nieminen0@gmail.com>
2024-01-06 00:27:54 +0200

    nvim-cmp improvements

--- init.lua
+++ init.lua
@@ -130,10 +130,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
   {
-    "numToStr/FTerm.nvim",
-    event = "VeryLazy",
-  },
-  {
     "rmagatti/auto-session",
     config = function(plugin, opts)
       require("auto-session").setup(opts)
@@ -311,9 +307,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
--- Terminal
-
-vim.keymap.set("n", "<leader>t", function() require("FTerm").toggle() end)
+-- -- Terminal
 vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
 -- Switch header/source
@@ -539,7 +533,9 @@ require('mason-lspconfig').setup()
 
 local servers = {
   clangd = {
-
+    arguments = {
+      "-header-insertion=never",
+    },
   },
   -- gopls = {},
   -- pyright = {},
@@ -604,15 +600,81 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+
+local format = function(_, item)
+  local ELLIPSIS_CHAR = '…'
+  local MAX_LABEL_WIDTH = 25
+  local MAX_KIND_WIDTH = 14
+
+  local get_ws = function(max, len)
+    return (" "):rep(max - len)
+  end
+
+  local content = item.abbr
+
+  if #content > MAX_LABEL_WIDTH then
+    item.abbr = vim.fn.strcharpart(content, 0, MAX_LABEL_WIDTH) .. ELLIPSIS_CHAR
+  else
+    item.abbr = content .. get_ws(MAX_LABEL_WIDTH, #content)
+  end
+
+  return item
+end
+
 cmp.setup {
   completion = {
     completeopt = 'menu,menuone,noinsert,preview',
     keyword_length = 1,
   },
+  formatting = {
+    fields = { "abbr", "menu", "kind" },
+    format = function(entry, item)
+      -- Define menu shorthand for different completion sources.
+      local menu_icon = {
+        nvim_lsp = "LSP ",
+        nvim_lua = "LUA ",
+        luasnip  = "SNIP",
+        buffer   = "BUFF",
+        path     = "PATH",
+      }
+      -- Set the menu "icon" to the shorthand for each completion source.
+      item.menu = menu_icon[entry.source.name]
+
+      -- Set the fixed width of the completion menu to 60 characters.
+      -- fixed_width = 20
+
+      -- Set 'fixed_width' to false if not provided.
+      fixed_width = fixed_width or false
+
+      -- Get the completion entry text shown in the completion window.
+      local content = item.abbr
+
+      -- Set the fixed completion window width.
+      if fixed_width then
+        vim.o.pumwidth = fixed_width
+      end
+
+      -- Get the width of the current window.
+      local win_width = vim.api.nvim_win_get_width(0)
+
+      -- Set the max content width based on either: 'fixed_width'
+      -- or a percentage of the window width, in this case 20%.
+      -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+      local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+      -- Truncate the completion entry text if it's longer than the
+      -- max content width. We subtract 3 from the max content width
+      -- to account for the "..." that will be appended to it.
+      if #content > max_content_width then
+        item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+      else
+        item.abbr = content .. (" "):rep(max_content_width - #content)
+      end
+      return item
+    end,
+  },
   mapping = cmp.mapping.preset.insert {
     ['<Esc>'] = cmp.mapping.close(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
@@ -620,24 +682,24 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
     },
-    -- ['<Tab>'] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expand_or_locally_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   else
-    --     fallback()
-    --   end
-    -- end, { 'i', 's' }),
-    -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.locally_jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, { 'i', 's' }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   },
   preselect = 'None',
   snippet = {
@@ -648,17 +710,8 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = "cmp-buffer" },
-    { name = "cmp-path" },
-  },
-  window = {
-    completion = {
-      border = "rounded",
-      scrolloff = 4,
-    },
-    documentation = {
-      border = "rounded",
-    },
+    { name = "buffer" },
+    { name = "path" },
   },
 }
 
